0409d569c7c63ecd4b441815030cb41539059c7b
ralfgraef <graef.ralf@gmail.com>
2019-02-10 19:58:05 +0100

    fetch data from databas and form submit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 
@@ -25,6 +26,11 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+// Body parser middelware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
 // Index Route
 app.get('/', (req, res) => {
   const title = 'Welcome!';
@@ -39,9 +45,48 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
+// Idea Index Page
+app.get('/ideas', (req, res) => {
+  Idea.find({})
+  .sort({date:'desc'})
+  .then(ideas => {
+    res.render('ideas/index', {
+      ideas: ideas
+    });
+  });
+});
+
 // Add Idea Form
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
+});
+
+// Process Form
+app.post('/ideas', (req, res) => {
+  let errors = [];
+  if(!req.body.title){
+    errors.push({text:'Add a title!'});
+  }
+  if(!req.body.details){
+    errors.push({text:'Add some details!'});
+  }
+  if(errors.length > 0) {
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    };
+    new Idea(newUser)
+    .save()
+    .then (idea => {
+      res.redirect('/ideas');
+    });
+  }
 });
 
 const port = 5000;
